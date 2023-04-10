ed33a06be25050bbc35b1c2306d91a72c4bfb6f5
Andrey Silva <andreysl2001@gmail.com>
2023-04-10 16:00:09 -0300

    BrowserRouter add

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import './App.css';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 // Components
 import Game from './Components/Game';
-import Card from './Components/Card';
-import { dragons } from './data/dragons';
 
 function App() {
-  /*
-  let luck = [{id:1}, {id:0}, {id: 4}, {id:7}, {id:9}]
-  let luckDragons = []
 
-  for(let i of luck){
-    for (let i2 of dragons) {
-      if(i.id === i2.id){
-        luckDragons.push(i2)
-      }
-    }
-  }
-
-  console.log(luckDragons)
-*/
   return (
     <div className="App">
-      <h1>Dragon Cards</h1>
-
-      <Game/>
-      {/*dragons.map((dragon) => (
-        <Card key={dragon.id} name={dragon.name} element={dragon.element} hp={dragon.hp_score} image={dragon.image} atk_fis={dragon.atk_fis} atk_ele={dragon.atk_ele} vel={dragon.vel} powers={dragon.powers} def={dragon.def} weak={dragon.weak}/>
-      ))*/}
-      
+      <BrowserRouter>
+        <h1>Dragon Cards</h1>
+        <Game/>
+      </BrowserRouter>
     </div>
   );
 }
